54a7fdf9f37c124ad550116d4b263c985c1b9371
Thaker13 <149239169+Thaker13@users.noreply.github.com>
2023-11-15 23:28:12 +0000

    Add files via upload

---
+++ DoWhile4.java
@@ -0,0 +1,15 @@
+package dowhile4;
+
+public class DoWhile4 {
+    public static void main (String[]args){
+
+        int i = 1;
+        do
+        {
+            System.out.println("4 *"+i+ "=" + i*4); i++;
+
+        }
+        while (i <= 10);
+    }
+
+}
